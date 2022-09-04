e10fd599cb8a2afcf3613e26fbf8d2c06ee49ddd
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-09-04 23:50:52 +0200

    Add cart

--- frontend/src/app/components/pages/cart-page/cart-page.component.ts
+++ frontend/src/app/components/pages/cart-page/cart-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/shared/models/cartitem';
+import { Cart } from 'src/app/shared/models/Carts';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent implements OnInit {
-
-  constructor() { }
+cart!:Cart
+  constructor(private cartService:CartService) {
+    this.cartService.getCartObservable().subscribe((cart)=>{
+      this.cart=cart
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  removeCartItem(cartItem:CartItem){
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+  changeQuantity(cartItem:CartItem,quantityString:string){
+   const quantity= parseInt(quantityString);
+   this.cartService.changeQuantity(cartItem.food.id,quantity);
+  }
 }
