f2a7b6c48c5e3d31a7fe2d085af7b33a2c0f10ad
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-09-10 17:12:51 +0200

    Login backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,9 @@
 import express from "express"
 import cors from 'cors'
-import { sampl_foods, sampl_tag } from "./data";
+import { sample_users, sampl_foods, sampl_tag } from "./data";
+import jwt from "jsonwebtoken"
 const app = express();
+app.use(express.json())
 app.use(cors(
    {
     credentials:true,
@@ -42,6 +44,30 @@ app.get('/api/foods/:foofId',(req,res)=>{
     const foods = sampl_foods.filter(food => food.id == foodId)
     res.send(foods)
 })
+
+app.post('/api/users/login',(req,res)=>{
+   // const body = req.body;
+    const {email , password} = req.body
+    const user = sample_users.find(user=>user.email === email &&
+        user.password === password)
+
+        if(user){
+          res.send(generateTokenRespons(user))
+        }else{
+            res.status(400).send("username or password is not correct")
+        }
+})
+
+const generateTokenRespons=(user:any)=>{
+const token = jwt.sign({
+    email: user.email,isAdmin:user.isAdmin
+},"Some Roundem Text",{
+ expiresIn:"30d"
+})
+
+user.token=token
+return user
+}
 
 const port=5000;
 app.listen((port),()=>{
