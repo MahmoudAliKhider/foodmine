f9af908158f9c36ebc069126806222d256cdf655
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-09-14 19:13:01 +0200

    button,validation,text_input

---
+++ frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
+const VALIDATORE_MESSAGE:any = {
+ require:"Should not be empty",
+ email:"Email is not Valid"
+}
+@Component({
+  selector: 'input-validation',
+  templateUrl: './input-validation.component.html',
+  styleUrls: ['./input-validation.component.css']
+})
+export class InputValidationComponent implements OnInit, OnChanges {
+@Input()
+  controle!:AbstractControl
+@Input()
+showErrorsWhen:boolean =true;
+errorMessages:string[]=[]
+  constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.cheackValidation()
+  }
+
+  ngOnInit(): void {
+    this.controle.statusChanges.subscribe(()=>{
+      this.cheackValidation()
+    })
+    this.controle.valueChanges.subscribe(()=>{
+      this.cheackValidation()
+    })
+
+  }
+  cheackValidation(){
+    const error = this.controle.errors
+    if(!error){
+      this.errorMessages=[]
+      return;
+    }
+
+    const errorKeys= Object.keys(error)
+    this.errorMessages=errorKeys.map(key=> VALIDATORE_MESSAGE[key])
+  }
+
+
+}
