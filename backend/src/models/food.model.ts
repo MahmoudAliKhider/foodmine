28888c6ded4adcef861db4238dae417ce9b62ba4
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-09-15 16:27:23 +0200

    Add Schema to food and user

--- backend/src/models/food.model.ts
+++ backend/src/models/food.model.ts
@@ -1,4 +1,4 @@
-import {Schema} from "mongoose"
+import {Schema,model} from "mongoose"
 
 export interface Food{
     id:string;
@@ -21,5 +21,16 @@ export const FoodSchema = new Schema<Food>(
         imageUrl: {type: String, required:true},
         origins: {type: [String], required:true},
         cookTime: {type: String, required:true}
+    },
+    {
+        toJSON:{
+            virtuals: true
+        },
+        toObject:{
+            virtuals: true
+        },
+        timestamps:true
     }
     )
+
+    export const FoodModels = model<Food> ('food',FoodSchema)
