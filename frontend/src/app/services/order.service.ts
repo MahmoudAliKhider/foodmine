3ad138e0fe53c517798af5e2a51168b71d56d62b
mahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-09-24 00:14:15 +0200

    order-track

--- frontend/src/app/services/order.service.ts
+++ frontend/src/app/services/order.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ORDER_CREATE_URL, ORDER_NEW_FOR_CURRENT_USER_URL, ORDER_PAY_URL } from '../shared/constants/url';
+import { ORDER_CREATE_URL, ORDER_NEW_FOR_CURRENT_USER_URL, ORDER_PAY_URL, ORDER_TRACK_URL } from '../shared/constants/url';
 import { Order } from '../shared/models/Orders';
 
 @Injectable({
@@ -20,5 +20,8 @@ export class OrderService {
 
   pay(order:Order):Observable<string>{
     return this.http.post<string>(ORDER_PAY_URL,order)
+  }
+  trackOrderById(id:number):Observable<Order>{
+    return this.http.get<Order>(ORDER_TRACK_URL +id)
   }
 }
